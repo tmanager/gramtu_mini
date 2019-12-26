13f5dd0f3af5f079db3366ce5b8b6c3a66bdbca1
zhangxiaodang <961590849@qq.com>
2019-12-26 08:21:13 +0800

    修改请求url

--- utils/config.js
+++ utils/config.js
@@ -1,5 +1,5 @@
 var config = {
-  serverAddress: 'http://172.16.7.105:8002/gramtu/web/',
+  serverAddress: 'http://172.16.7.88:8002/gramtu/mini/',
   SUCCESS: "0000",
   articleAddress: 'http://172.16.7.105:8006/template'
 }
