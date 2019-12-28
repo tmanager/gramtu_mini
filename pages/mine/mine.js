c2321fe397a4ddb866198a2bf8f3d35ed57bbf21
tmanager <961590849@qq.com>
2019-12-28 15:59:07 +0800

    登录及首页接口文档提交

--- pages/mine/mine.js
+++ pages/mine/mine.js
@@ -9,7 +9,8 @@ Page({
    * 页面的初始数据
    */
   data: {
-    showModal: false,//定义登录弹窗
+    showModal: false,//定义登录弹窗,
+    userInfoScope: 0,
     userInfo: app.globalData.userInfo,
     mark:5,
     mode: [
@@ -25,11 +26,23 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    var that = this;
     var register = wx.getStorageSync("register");
     var mark = wx.getStorageSync("mark");
     this.setData({
       mark: mark
     });
+    // 获取用户信息
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          that.setData({
+            userInfoScope: 1
+          });
+        }
+      }
+    })
     if (register == 1 && app.globalData.userInfo == null){
       wx.getUserInfo({
         success: res => {
@@ -104,6 +117,19 @@ Page({
       wx.setStorageSync("userinfo", e.detail.userInfo);
       that.showDialogBtn();//调用一键获取手机号弹窗（自己写的）
     }
+  },
+  getUserInfoScope: function(e){
+    var that = this;
+    wx.getUserInfo({
+      success: res => {
+        // 可以将 res 发送给后台解码出 unionId
+        wx.setStorageSync("userinfo", e.detail.userInfo);
+        that.showDialogBtn();//调用一键获取手机号弹窗（自己写的）
+      },
+      complete: res => {
+        console.info("wx.userInfo:" + JSON.stringify(res));
+      }
+    })
   },
   // 显示一键获取手机号弹窗
   showDialogBtn: function () {
